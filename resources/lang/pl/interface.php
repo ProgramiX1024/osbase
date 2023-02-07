eec013078115633c9c9e3d81acae3936d3a70601
ProgramiX 1024 <115416102+ProgramiX1024@users.noreply.github.com>
2023-02-07 16:15:26 +0100

    Update interface.php

--- resources/lang/pl/interface.php
+++ resources/lang/pl/interface.php
@@ -2,7 +2,7 @@
 
 return[
 
-	'hello' => 'Witaj na osBase.pl!',
+	'hello' => 'Witaj w bazie systemów operacyjnych!',
 	'desc' => 'Możesz tutaj pobrać pliki ISO Windowsów, pakietów Office i dystrybucji Linuxa. Zachęcamy do pobierania! ',
 	'office' => 'Microsoft 365 - wcześniej Office',
 	'linux' => 'Dystrybucje Linuxa',
@@ -11,17 +11,14 @@ return[
 	'created' => 'Strona stworzona z pomocą skryptu osBase: <a href="https://github.com/ProgramiX1024/osbase">Zobacz na GitHubie</a>',
 	'home' => 'Strona główna',
 	'about' => 'O nas',
-	'announcements' => 'Ogłoszenia - Telegram',
+	'announcements' => 'Ogłoszenia',
 	'timers' => 'Odliczanie do końca wsparcia systemów',
-	'money' => 'Datki na osBase.pl',
+	'money' => 'Datki na rozwój',
 	'search' => 'Szukaj',
 	'personname' => 'Tu możesz umieścić opis danej osoby. Możesz także dodać kolejne opisy w pliku interface.php w /resources/lang/pl/',
 	'timerdesc' => 'Możesz tutaj znaleźć linki do odliczania końca wsparcia systemów Windows 8.1 oraz Windows 10.',
 	'timerwin8' => 'Odliczanie do końca wsparcia Windowsa 8.1',
 	'timerwin10' => 'Odliczanie do końca wsparcia Windowsa 10',
-	'money3' => 'Aby ta strona pozostała w pełni darmowa, prosimy o wsparcie naszego serwisu. <br> W całości kwota uzbierana przez wsparcie będzie pokrywała koszty opłacenia domeny i serwerów należących do osBase. ',
-	'moneyheader' => 'Jak możesz wesprzeć osBase.pl?',
-	'money2' => 'Możesz nas wesprzeć używając jednej z trzech metod: ',
 	'searchresults' => 'Wyniki wyszukiwania dla',
 	
 	
