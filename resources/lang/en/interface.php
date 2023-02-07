4eb0323bfc52d111f7f04a40f2f9437744130d09
ProgramiX 1024 <115416102+ProgramiX1024@users.noreply.github.com>
2023-02-07 16:16:11 +0100

    Update interface.php

--- resources/lang/en/interface.php
+++ resources/lang/en/interface.php
@@ -2,7 +2,7 @@
 
 return[
 
-	'hello' => 'Hello on osBase.pl!',
+	'hello' => 'Hello on OS base!',
 	'desc' => 'In this website you can download ISO images of Windows, Microsoft 365 and Linux distributions. Welcome to downloading!',
 	'office' => 'Microsoft 365 - formerly Office',
 	'linux' => 'Linux distributions',
@@ -13,15 +13,12 @@ return[
 	'about' => 'About us',
 	'announcements' => 'Announcements - Telegram',
 	'timers' => 'Counting to end of support OSes',
-	'money' => 'Donations to osBase.pl',
+	'money' => 'Donations to project',
 	'search' => 'Search',
 	'personname' => 'Here you can add description about person. You can add more translates to interface.php in /resources/lang/en/',
 	'timerdesc' => 'In this place you can find links to counting for end of suport OSes: Windows 8.1 and Windows 10.',
 	'timerwin8' => 'Counting for Windows 8.1 end of support',
 	'timerwin10' => 'Counting for Windows 10 end of support',
-	'money3' => 'To keep this website absolutely free, please support our service.<br>All raised by crowdfunding money will be spend to pay for domain and our servers.',
-	'moneyheader' => 'How you can support osBase.pl?',
-	'money2' => 'You can support us using one from 3 methods:',
 	'searchresults' => 'Search results for',
 	
 ];
