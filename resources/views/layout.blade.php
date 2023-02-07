aba64a8c034d890038244655e6e4db06f60cd316
ProgramiX 1024 <115416102+ProgramiX1024@users.noreply.github.com>
2023-02-07 16:47:35 +0100

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">osBase.pl</a>
+        <a class="navbar-brand" href="/">Nazwa bazy</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -40,7 +40,7 @@
             <a class="nav-link" href="/about-us">{{ __('interface.about') }}</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="https://t.me/osbasepl">{{ __('interface.announcements') }}</a>
+            <a class="nav-link" href="#">{{ __('interface.announcements') }}</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href="/timers">{{ __('interface.timers') }}</a>
